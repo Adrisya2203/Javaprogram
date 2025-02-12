4a220065ae611e5cbb4f66e4353da093375115d0
Adrisya <147034695+Adrisya2203@users.noreply.github.com>
2025-02-12 10:49:40 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition
+{
+public static void main(String[]args)
+{
+int x;
+x=11+5;
+System.out .println("sum is "+x);
+}
+}
---
+++ No.java
@@ -0,0 +1,19 @@
+public class No
+{
+public static void main (String[]args)
+{
+int a=10;
+if(a>0)
+{
+System.out.println("it is a positive no");
+}
+else if(a==0)
+{
+System.out.println("it is Zero");
+}
+else
+{
+System.out.println("it is a negative no");
+}
+}
+}
